300a15717b3f84aa5fcad0fb112223844b32e6ce
Larrygf02 <raulgf_02@hotmail.com>
2019-08-21 12:12:33 -0500

    Update user

--- src/index.js
+++ src/index.js
@@ -64,5 +64,39 @@ function Login() {
 
     });
 }
+
+function update(username, password){
+    var attributeList = [];
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({
+        Name: "custom:scope",
+        Value: "some new value"
+    }));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({
+        Name: "name",
+        Value: "some new value"
+    }));
+
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username: username,
+        Password: password,
+    });
+
+    var userData = {
+        Username: username,
+        Pool: userPool
+    };
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+    cognitoUser.updateAttributes(attributeList, (err, result) => {
+        if (err) {
+            //handle error
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+    });
+}
+
+update('raulgf_02@hotmail.com', 'SamplePassword_123')
 //RegisterUser();
 //Login()
